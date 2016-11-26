96a7bf837cea5656c82d3ce4299bff1cbc3adcde
Salihan <salihan04@gmail.com>
2016-11-26 21:11:41 +0800

    Setup basic scene with Three.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var scene = new THREE.Scene(),
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
+    renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
+
+render();
